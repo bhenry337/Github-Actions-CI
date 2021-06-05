0fdd9695610c2ece89743acad1531c3f8c99e3d3
bhenry337 <72040662+bhenry337@users.noreply.github.com>
2021-06-05 16:58:01 -0700

    added test code for mult and div

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ class TestCase(unittest.TestCase):
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
+    def test_mult_1(self):
+        self.assertEqual(example.multiply(3, 4), 12)
+
+    def test_div_1(self):
+        self.assertEqual(example.div(6, 2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
